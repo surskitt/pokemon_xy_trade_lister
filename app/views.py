135d855b6e2c5766b94d88e7a71e9738ac85e25a
sharktamer <priestoftime@gmail.com>
2013-12-21 00:28:42 +0000

    Initial creation of index.html and included mon_detail.html

--- app/views.py
+++ app/views.py
@@ -5,4 +5,18 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("base.html")
+    trades = [
+        {
+            'id': '045',
+            'species': 'Vileplume'
+        },
+        {
+            'id': '185',
+            'species': 'Sudowoodo'
+        },
+        {
+            'id': '417',
+            'species': 'Pachirisu'
+        }
+    ]
+    return render_template("index.html", trades=trades)
