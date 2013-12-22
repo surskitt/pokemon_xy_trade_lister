5b9b503300299f22774b295d843e467ccfb20ee2
sharktamer <priestoftime@gmail.com>
2013-12-22 12:34:57 +0000

    Create LoginForm class

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,12 @@
 from flask.ext.wtf import Form
-from wtforms import TextField
+from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 
 
 class SearchForm(Form):
     search = TextField('search', validators=[Required()])
+
+
+class LoginForm(Form):
+    openid = TextField('openid', validators=[Required()])
+    remember_me = BooleanField('remember_me', default=False)
