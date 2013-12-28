e8b829a81927c0a0f9b15b92d32f874fd7f75e32
sharktamer <priestoftime@gmail.com>
2013-12-28 15:26:32 +0000

    Added coverage testing

--- tests.py
+++ tests.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+from coverage import coverage
+cov = coverage(branch=True, omit=['flask/*', 'tests.py'])
+cov.start()
+
 import os
 import unittest
 
@@ -7,11 +11,14 @@ from config import basedir
 from app import app, db
 from app.models import User
 
+
 class TestCase(unittest.TestCase):
+
     def setUp(self):
         app.config['TESTING'] = True
         app.config['CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(basedir, 'test.db')
         self.app = app.test_client()
         db.create_all()
 
@@ -20,18 +27,18 @@ class TestCase(unittest.TestCase):
         db.drop_all()
 
     def test_avatar(self):
-        u = User(nickname = 'john', email = 'john@example.com')
+        u = User(nickname='john', email='john@example.com')
         avatar = u.avatar(128)
         expected = 'http://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6'
         assert avatar[0:len(expected)] == expected
 
     def test_make_unique_nickname(self):
-        u = User(nickname = 'john', email = 'john@example.com')
+        u = User(nickname='john', email='john@example.com')
         db.session.add(u)
         db.session.commit()
         nickname = User.make_unique_nickname('john')
         assert nickname != 'john'
-        u = User(nickname = nickname, email = 'susan@example.com')
+        u = User(nickname=nickname, email='susan@example.com')
         db.session.add(u)
         db.session.commit()
         nickname2 = User.make_unique_nickname('john')
@@ -39,4 +46,14 @@ class TestCase(unittest.TestCase):
         assert nickname2 != nickname
 
 if __name__ == '__main__':
-    unittest.main()
+    try:
+        unittest.main()
+    except:
+        pass
+    cov.stop()
+    cov.save()
+    print "\n\nCoverage Report:\n"
+    cov.report()
+    print "HTML version: " + os.path.join(basedir, "tmp/coverage/index.html")
+    cov.html_report(directory='tmp/coverage')
+    cov.erase()
