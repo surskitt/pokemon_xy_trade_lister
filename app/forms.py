71f1444c3a7c75cbfc46b5ac94cb51bd4bdfbe6f
sharktamer <priestoftime@gmail.com>
2013-12-29 19:07:00 +0000

    Change IV selection default to 31

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField, TextAreaField, SelectField, IntegerField, DecimalField, SelectMultipleField
-from wtforms.validators import Required, Length, NumberRange
+from wtforms import TextField, BooleanField, TextAreaField, SelectField, IntegerField, SelectMultipleField
+from wtforms.validators import Required, Length
 from models import User
 from forms_selectors import national_dex, natures, abilities, moves
 
@@ -15,8 +15,9 @@ class LoginForm(Form):
 
 
 class EditUserForm(Form):
-    nickname = TextField('nickname', validators = [Required()])
-    about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
+    nickname = TextField('nickname', validators=[Required()])
+    about_me = TextAreaField(
+        'about_me', validators=[Length(min=0, max=140)])
 
     def __init__(self, original_nickname, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
@@ -27,24 +28,27 @@ class EditUserForm(Form):
             return False
         if self.nickname.data == self.original_nickname:
             return True
-        user = User.query.filter_by(nickname = self.nickname.data).first()
-        if user != None:
-            self.nickname.errors.append('This nickname is already in use. Please choose another one.')
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user is not None:
+            self.nickname.errors.append(
+                'This nickname is already in use. Please choose another one.')
             return False
         return True
 
+
 class NewTradeForm(Form):
-    iv_tup = [(str(i),(i)) for i in range(0,32) + ['?']]
+    iv_tup = [(str(i), (i)) for i in range(0, 32) + ['?']]
 
     species = SelectField('species', choices=national_dex, default=1)
-    gender = SelectField('gender', choices=[('Male', 'Male'), ('Female', 'Female')], default=1)
+    gender = SelectField(
+        'gender', choices=[('Male', 'Male'), ('Female', 'Female')], default=1)
     count = IntegerField('count', default=1)
     nature = SelectField('nature', choices=natures, default=1)
     ability = SelectField('ability', choices=abilities, default=1)
-    iv_hp = SelectField('iv_hp', choices=iv_tup, default=0)
-    iv_atk = SelectField('iv_atk', choices=iv_tup, default=0)
-    iv_def = SelectField('iv_def', choices=iv_tup, default=0)
-    iv_spa = SelectField('iv_spa', choices=iv_tup, default=0)
-    iv_spd = SelectField('iv_spd', choices=iv_tup, default=0)
-    iv_spe = SelectField('iv_spe', choices=iv_tup, default=0)
+    iv_hp = SelectField('iv_hp', choices=iv_tup, default=31)
+    iv_atk = SelectField('iv_atk', choices=iv_tup, default=31)
+    iv_def = SelectField('iv_def', choices=iv_tup, default=31)
+    iv_spa = SelectField('iv_spa', choices=iv_tup, default=31)
+    iv_spd = SelectField('iv_spd', choices=iv_tup, default=31)
+    iv_spe = SelectField('iv_spe', choices=iv_tup, default=31)
     moves = SelectMultipleField('moves', choices=moves, default=1)
