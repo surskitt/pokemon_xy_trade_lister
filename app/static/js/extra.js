d37a78e6d6bd820e7550f9af6cd5344b6e7f04d7
sharktamer <priestoftime@gmail.com>
2013-12-30 22:57:43 +0000

    Add javascript to toggle editing user info

--- app/static/js/extra.js
+++ app/static/js/extra.js
@@ -1,11 +1,17 @@
 function set_openid(openid, pr)
 {
-    u = openid.search('<username>')
-    if (u != -1) {
-        // openid requires username
-        user = prompt('Enter your ' + pr + ' username:')
-        openid = openid.replace('<username>', user)
-    }
-    form = document.forms['login'];
-    form.elements['openid'].value = openid
+	u = openid.search('<username>')
+	if (u != -1) {
+			// openid requires username
+			user = prompt('Enter your ' + pr + ' username:')
+			openid = openid.replace('<username>', user)
+	}
+	form = document.forms['login'];
+	form.elements['openid'].value = openid
+}
+
+function toggleAboutEdit()
+{
+	$("#userInfo").toggle()
+	$("#userInfoEdit").toggle()
 }
