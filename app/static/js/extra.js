9c436469690af143353dff5ac1f16496605f5a36
sharktamer <priestoftime@gmail.com>
2013-12-31 02:35:38 +0000

    Included the toastr jquery plugin

--- app/static/js/extra.js
+++ app/static/js/extra.js
@@ -15,3 +15,18 @@ function toggleAboutEdit()
 	$("#userInfo").toggle()
 	$("#userInfoEdit").toggle()
 }
+
+toastr.options = {
+			"closeButton": true,
+			"debug": false,
+			"positionClass": "toast-bottom-right",
+			"onclick": null,
+			"showDuration": "300",
+			"hideDuration": "1000",
+			"timeOut": "5000",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+			"showMethod": "fadeIn",
+			"hideMethod": "fadeOut"
+		}
